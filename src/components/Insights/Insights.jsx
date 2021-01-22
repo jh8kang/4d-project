46bb671fa43cb4b314698be3c893ed95889f72f6
Ju Hee Kang <amykang@Jus-MacBook-Pro.local>
2021-01-22 12:50:55 -0800

    initial commit

--- src/components/Insights/Insights.jsx
+++ src/components/Insights/Insights.jsx
@@ -29,7 +29,6 @@ const applicationKey = "1dtGOdhNfXviwBcAMjesOeMbtpK1jmBP";
 const applicationClientSecret = "byTVZU5BIWCPkeKQ";
 const token = 'v2/MWR0R09kaE5mWHZpd0JjQU1qZXNPZU1idHBLMWptQlAvMjkyMTc2MzE3L2N1c3RvbWVyLzMvU09ZRDRCaWd6aGZ0ZzctbXhiWFJoUnJ3dURoZFQtOGVrZHdVZ0xVMTh5SXkwaFdqLUlEM0xZZElFSjJtc3B5Z2xvYXJaYkJWVGNLbUNSQ0trQUZwRGJGdFpCeW1BSDJMcDktR0JJTTNzYjNJWFNUaUtmUjdqZ0JzMUxsRXRFbjdia1JneFN0MWpwdEVKcXc0ajVKbUNZTGI2RHdyak9YeFlDRThOVFpWS0dFdEJRX0w2R2dRSjVZSDBRaFk0Ykp1R1pnWThNakVaYnpkZjNXXzJCM3BBdw'
 
-
 class Insights extends Component {
     constructor() {
         super();
@@ -56,7 +55,6 @@ class Insights extends Component {
                                     return <img key={uuid()} src={image} className="insight__img"/>
                                 })}
                             </div>
-
                         </div>
             
                         <div className="insight">
